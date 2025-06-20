31ea91c7021d5a53fa2a790ab67490553220d493
Dam1230 <dkssud4111@gmail.com>
2025-06-20 12:58:24 +0900

    Feat: 플레이어 이동 구현
    현재는 PlayerStats를 참조하지 않고 이동을 구현하며, 추후 스탯 구현 시 연동 예정

---
+++ Assets/CMS/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 5f;
+
+    private Rigidbody2D _rb;
+    private Vector2 _moveInput;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _moveInput.x = Input.GetAxisRaw("Horizontal");
+        _moveInput.y = Input.GetAxisRaw("Vertical");
+        _moveInput.Normalize();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.MovePosition(_rb.position + _moveInput * _moveSpeed * Time.fixedDeltaTime);
+    }
+}
