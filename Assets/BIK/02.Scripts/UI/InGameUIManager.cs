c77d5ec4097470fe056270e0e1021bc15ed0bc04
frhug922 <dlsrnjsl3@gmail.com>
2025-06-25 17:43:52 +0900

    [Feat] Add InGame UIs
    인게임 UI 추가 및 IngameScene 수정

---
+++ Assets/BIK/02.Scripts/UI/InGameUIManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameUIManager : MonoBehaviour
+{
+    #region 
+
+    [SerializeField] private UIBase[] _uis;
+
+    #endregion 
+}
