4948e4edc350eaf791b68174ae3ddb2cc81b34ff
jongwon1205 <haegol5009@gmail.com>
2025-06-30 15:11:27 +0900

    [Feat] Add CollectionPanelController
    도감 UI 여닫이 기능 추가

---
+++ Assets/PJW/Script/Collection/CollectionPanelController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectionPanelController : MonoBehaviour
+{
+    [SerializeField] private GameObject collectionPanel;
+
+    private bool isOpen = false;
+
+    private void Start()
+    {
+        if (collectionPanel != null)
+            collectionPanel.SetActive(isOpen);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            ToggleCollectionPanel();
+        }
+    }
+
+    private void ToggleCollectionPanel()
+    {
+        if (collectionPanel == null) return;
+
+        isOpen = !isOpen;
+        collectionPanel.SetActive(isOpen);
+    }
+}
