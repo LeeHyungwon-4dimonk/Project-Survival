ceface7185adcbbee938fad3dab274a057bfd8ce
t3qquq <63445961+t3qquq@users.noreply.github.com>
2025-07-01 16:43:20 +0900

    Revert "[Test] refactored useless codes"
    This reverts commit 699b303387b5250e5a88f357a55c569995357f48.

--- Assets/SJW/Script/UI/FarmingUIController.cs
+++ Assets/SJW/Script/UI/FarmingUIController.cs
@@ -12,6 +12,17 @@ public class FarmingUIController : MonoBehaviour
 
     [SerializeField] private float panelAnchoredPosY = 611f;
 
+    private void OnValidate()
+    {
+        if (interactionPanel != null)
+        {
+            RectTransform rect = interactionPanel.GetComponent<RectTransform>();
+            Vector2 anchored = rect.anchoredPosition;
+            anchored.y = panelAnchoredPosY;
+            rect.anchoredPosition = anchored;
+        }
+    }
+
     public void Show(string message, Transform target)
     {
         interactionPanel.SetActive(true);
@@ -21,5 +32,21 @@ public class FarmingUIController : MonoBehaviour
     public void Hide()
     {
         interactionPanel.SetActive(false);
+    }
+
+    public void ShowHoldProgressBar(float fillAmount)
+    {
+        if (holdProgressBarImage != null)
+        {
+            holdProgressBarImage.fillAmount = Mathf.Clamp01(fillAmount);
+        }
+    }
+
+    public void ResetProgressBar()
+    {
+        if (holdProgressBarImage != null)
+        {
+            holdProgressBarImage.fillAmount = 0f;
+        }
     }
 }
