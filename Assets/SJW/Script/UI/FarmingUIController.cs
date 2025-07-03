63b8ed66e96cb3aefe22244b05ad3a36dd0a56a7
t3qquq <63445961+t3qquq@users.noreply.github.com>
2025-07-03 12:10:03 +0900

    [Fix] player headup interaction UI
    상호작용UI 위치를 화면 가운데 -> 플레이어 위치 기준으로 변경

--- Assets/SJW/Script/UI/FarmingUIController.cs
+++ Assets/SJW/Script/UI/FarmingUIController.cs
@@ -2,26 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
 
 public class FarmingUIController : MonoBehaviour
 {
     [SerializeField] private GameObject interactionPanel;
     [SerializeField] private TMP_Text interactionText;
-    [SerializeField] private Image holdProgressBarImage; // Ãß°¡µÊ
-
-    [SerializeField] private float panelAnchoredPosY = 611f;
-
-    private void OnValidate()
-    {
-        if (interactionPanel != null)
-        {
-            RectTransform rect = interactionPanel.GetComponent<RectTransform>();
-            Vector2 anchored = rect.anchoredPosition;
-            anchored.y = panelAnchoredPosY;
-            rect.anchoredPosition = anchored;
-        }
-    }
 
     public void Show(string message, Transform target)
     {
@@ -32,21 +17,5 @@ public class FarmingUIController : MonoBehaviour
     public void Hide()
     {
         interactionPanel.SetActive(false);
-    }
-
-    public void ShowHoldProgressBar(float fillAmount)
-    {
-        if (holdProgressBarImage != null)
-        {
-            holdProgressBarImage.fillAmount = Mathf.Clamp01(fillAmount);
-        }
-    }
-
-    public void ResetProgressBar()
-    {
-        if (holdProgressBarImage != null)
-        {
-            holdProgressBarImage.fillAmount = 0f;
-        }
     }
 }
